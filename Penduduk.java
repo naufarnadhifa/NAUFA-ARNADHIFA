f572769e448b0744822065a20b25cfbd70e0c7ef
naufarnadhifa <naufardhff@gamil.com>
2022-11-16 10:59:03 +0700

    quis1_c_3069

---
+++ Penduduk.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package praktikum.quis1_c_3069;
+
+/**
+ *
+ * @author NAUFA ARNADHIFA
+ */
+public class Penduduk {
+     String NIK, nama, alamat;
+    int umur;
+    
+    public void tampilDataPenduduk(){
+        System.out.println("NIK          : " + NIK);
+        System.out.println("Nama         : " + nama);
+        System.out.println("Umur         : " + umur);
+        System.out.println("Alamat       : " + alamat);
+    }
+}
+    
+
